618a11a19d57d8b486d0603acbe07d02d95b1eb0
Jeffrey Platt <jsplatt@gmail.com>
2021-11-22 16:05:59 -0500

    doc center started

--- pages/DocumentCenter.js
+++ pages/DocumentCenter.js
@@ -3,11 +3,19 @@ import Header from './components/Header'
 import tw from 'tailwind-styled-components'
 
 const DocumentCenter = () => {
+
     return (
     <>
     <Header/>
         <Wrapper>
             Document Center Page
+            <ol>
+                <li><h3>TODO-1</h3></li>
+                <li><h3>TODO-2</h3></li>
+                <li><h3>TODO-3</h3></li>
+                <li><h3>TODO-4</h3></li>
+                <li><h3>TODO-5</h3></li>
+            </ol>
         </Wrapper>
     </>
     )
