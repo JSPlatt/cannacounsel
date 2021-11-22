392c5eab1ca32816d7a56b732e9e976873d8332e
Jeffrey Platt <jsplatt@gmail.com>
2021-11-22 16:18:25 -0500

    ...

--- pages/DocumentCenter.js
+++ pages/DocumentCenter.js
@@ -12,12 +12,12 @@ const DocumentCenter = () => {
             
             <Todo>
                 <ol class="list-decimal">
-                <li>TODO-1</li>
-                <li>TODO-2</li>
-                <li>TODO-3</li>
-                <li>TODO-4</li>
-                <li>TODO-5</li>
-            </ol>
+                    <li>TODO-1</li>
+                    <li>TODO-2</li>
+                    <li>TODO-3</li>
+                    <li>TODO-4</li>
+                    <li>TODO-5</li>
+                </ol>
             </Todo>
             
         </Wrapper>
@@ -28,9 +28,9 @@ const DocumentCenter = () => {
 export default DocumentCenter
 
 const Wrapper = tw.div`
-bg-gray-100 h-screen flex flex-col items-center
+bg-gray-100 h-screen flex flex-col items-center text-2xl
 `
 
 const Todo = tw.div`
-flex flex-col justify-between items-center
+flex flex-col justify-between items-center text-green-700 text-xl py-4 px-4 bg-red-500 
 `
